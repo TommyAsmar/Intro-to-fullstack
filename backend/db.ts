05399454a3c3525cc6d4a1fc8d7eecba0c85a6cd
Tommy El Asmar <tommyelasmar@gmail.com>
2025-06-08 14:07:14 +0300

    Stop tracking db.ts

--- backend/db.ts
+++
@@ -1,9 +0,0 @@
-// src/db.ts
-import mysql from 'mysql2/promise';
-
-export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Tommy2004.', // your password
-  database: 'ts_demo'
-});
